f568abb0524d8d2e41bd12ccce75a10a413878d2
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2023-02-16 02:19:51 +0000

    Teleport and Dominant Hand
    Teleport works according to the dominant hand.

--- Assets/Scripts/CheckXRRigStatus.cs
+++ Assets/Scripts/CheckXRRigStatus.cs
@@ -7,9 +7,14 @@ public class CheckXRRigStatus : MonoBehaviour
     public HandAndMovement handStatus;
     public bool teleportMovementStatus = false;
 
+    public bool isLeftDominantHand;
+    public bool isRightDominantHand;
+
     private void Start()
     {
         teleportMovementStatus = false;
+        isLeftDominantHand = false;
+        isRightDominantHand = false;
         handStatus = GameObject.FindObjectOfType<HandAndMovement>();
     }
     public void IsTeleportMovementSelected()
@@ -18,5 +23,11 @@ public class CheckXRRigStatus : MonoBehaviour
         {
             teleportMovementStatus = true;
         }
+        else
+        {
+            teleportMovementStatus = false;
+        }
+        isLeftDominantHand = handStatus.leftHandUser;
+        isRightDominantHand = handStatus.righttHandUser;
     }
 }
