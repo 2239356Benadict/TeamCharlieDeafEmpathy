383498e0c346bfb7ef6be42457550ab5e7347dff
Caitlinb2021 <115563114+Caitlinb2021@users.noreply.github.com>
2023-03-02 17:46:46 +0000

    Trigger Doctor Animations when Player entetrs the doctors office in caitlins scene

---
+++ Assets/Scripts/DoctorandPatientAnimatorScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+
+public class DoctorandPatientAnimatorScript : MonoBehaviour
+{
+    public Animator anim;
+    public BoxCollider DoctorsRoomCollider;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        DoctorsRoomCollider = gameObject.AddComponent(typeof(BoxCollider)) as BoxCollider;
+        DoctorsRoomCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.Play("Typing");
+        }
+    }
+}
+
