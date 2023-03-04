55fc5c742f982bb7d8b38b0d0735b066e3b841bb
Benadict <115377654+2239356Benadict@users.noreply.github.com>
2023-03-04 08:02:20 +0000

    Vuforia10

---
+++ Assets/Scripts/FinalScenario.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalScenario : MonoBehaviour
+{
+    public GameObject endPanel;
+    [SerializeField]
+    private AudioSource audioSource;
+    public int timeToUIShowUp;
+
+
+    private void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(EnableTheUI());
+        }
+    }
+
+    IEnumerator EnableTheUI()
+    {
+        yield return new WaitForSeconds(audioSource.clip.length);
+        endPanel.SetActive(true);
+    }
+}
